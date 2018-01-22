c532ae68704a65dbaeb379cc7741b029761ecd7b
GuardOfDawn <986721937@qq.com>
2018-01-22 19:39:42 +0800

    initial commit

---
+++ src/main/java/cn/zy/GroupAnswering/GroupAnsweringApplication.java
@@ -0,0 +1,12 @@
+package cn.zy.GroupAnswering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupAnsweringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupAnsweringApplication.class, args);
+	}
+}
